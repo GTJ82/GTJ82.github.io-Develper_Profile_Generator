ba70f2f0eca59b7c63689edb50c132205a279754
Josh Eskew <jeskew82@gmail.com>
2019-11-10 13:55:57 -0500

    Was able to get user location and Github avatar to print to .md file

--- index.js
+++ index.js
@@ -18,20 +18,42 @@ inquirer.prompt([
     axios.get("https://api.github.com/users/" + info.name)
         .then(function (response) {
             console.log(response.data);
-            
-                let locate = response.data.location
-            
-                fs.writeFile("user.md", "### " + locate + " is the users location", function (err) {
-                    if (err) {
-                        return (console.log(err));
-                    }
-                    console.log("yay!");
-                    
-        });
-    })
-    
-    
-        
-    });
 
-          
+            let locate = response.data.location
+
+
+            fs.writeFile("user.md", "### " + locate + " is the users location", function (err) {
+                if (err) {
+                    return (console.log(err));
+                }
+
+                // console.log("yay!");
+
+            });
+
+        })
+        .then(function (response) {
+            axios.get("https://api.github.com/users/" + info.name)
+                .then(function (response) {
+                    console.log(response.data);
+
+                    let image = response.data.avatar_url
+
+                    fs.appendFile("user.md", "\n" + image, function (err) {
+
+
+                        if (err) {
+                            return console.log(err);
+                        }
+
+                        console.log("Success!");
+
+                    });
+                });
+
+        })
+
+
+
+});
+
--- user.md
+++ user.md
@@ -1,1 +1,2 @@
 ### Charlotte, NC is the users location
+https://avatars3.githubusercontent.com/u/53836954?v=4
